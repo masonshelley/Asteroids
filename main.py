b518d43547bd8483c810af2654815453bfd1e54b
Mason Shelley <masongm.shelley@gmail.com>
2025-01-02 08:43:13 -0600

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+def main():
+    print(f"Starting asteroids!")
+
+if __name__ == "__main__":
+    main()
