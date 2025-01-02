1fd447055dc93a97798754113f143cf9466e52bf
masonshelley <masongm.shelley@gmail.com>
2025-01-02 08:49:45 -0600

    update

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+# import everything from a module
+# into the current file
+from module import constants
 def main():
     print(f"Starting asteroids!")
+    print(f"Screen width: 1280")
+    print(f"Screen height: 720")
 
 if __name__ == "__main__":
     main()
